b11d930bf8e5b8b009e1bda556be7d19a70f16bf
chenggang.zhang <chenggang.zhang@ikang.com>
2025-03-22 16:26:23 +0800

    1.0.0

--- zcgtoast/src/main/java/com/zcg/zcgtoast/ZcgToast.kt
+++ zcgtoast/src/main/java/com/zcg/zcgtoast/ZcgToast.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
  */
 class ZcgToast {
 
+
     fun showShortToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
