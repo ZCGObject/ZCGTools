cd5f5b41da610c1dbc6ec87e475a9e00f74a93f8
chenggang.zhang <chenggang.zhang@ikang.com>
2025-03-22 16:23:16 +0800

    Initial commit

---
+++ zcgtoast/src/main/java/com/zcg/zcgtoast/ZcgToast.kt
@@ -0,0 +1,22 @@
+package com.zcg.zcgtoast
+
+import android.content.Context
+import android.widget.Toast
+/**
+ * 项目名称：ZCGTools
+ * 描述：
+ * 创建人：ZCG
+ * 创建时间：2025/3/22
+ */
+class ZcgToast {
+
+    fun showShortToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // 长时间吐司
+    fun showLongToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+}
